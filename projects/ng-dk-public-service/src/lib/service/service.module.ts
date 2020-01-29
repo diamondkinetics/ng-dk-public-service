49e2e6ffb747a6375edfba23aa64f0a5036105c9
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-01-29 11:29:21 -0500

    Removed empty decorator properties. Added ServiceModuleV3 as a child module.

--- projects/ng-dk-public-service/src/lib/service/service.module.ts
+++ projects/ng-dk-public-service/src/lib/service/service.module.ts
@@ -2,19 +2,21 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthModule } from './http/auth/auth.module';
+import { ServiceModuleV3 } from './http/v3/service.module.v3';
 
 @NgModule({
-	imports: [
-		CommonModule,
-		AuthModule
-	],
-	declarations: [],
-	exports: []
+	imports: [CommonModule],
+	exports: [
+		AuthModule,
+		ServiceModuleV3
+	]
 })
 export class ServiceModule {
+
 	constructor(@Optional() @SkipSelf() parentModule: ServiceModule) {
 		if (parentModule) {
 			throw new Error('ServiceModule is already loaded. Import it in the AppModule only.');
 		}
 	}
+
 }
