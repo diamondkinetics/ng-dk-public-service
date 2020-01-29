f5dcd867b59c5fd3c48e11cbcb4daf990755b173
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-01-29 11:25:56 -0500

    Added abstract resource and sensor session service classes.

---
+++ projects/ng-dk-public-service/src/lib/service/http/abstract-resource.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { AbstractSyncableDTOV3 } from '@diamondkinetics/dk-public-dto-ts';
+import { AbstractSyncableDTOV2 } from '@diamondkinetics/dk-public-dto-ts';
+
+export abstract class AbstractResourceService<T extends AbstractSyncableDTOV2|AbstractSyncableDTOV3> {
+
+	constructor(protected http: HttpClient, protected version: number, protected endpoint: string) {}
+
+	public create(item: T, params?: {}): Observable<T> {
+		return this.http.post<T>(`/${this.getVersionString()}/${this.endpoint}`, item, { params });
+	}
+
+	public readForActingUser(params?: {}): Observable<T> {
+		return this.http.get<T>(`/${this.getVersionString()}/${this.endpoint}`, { params });
+	}
+
+	public read(uuid: string, params?: {}): Observable<T> {
+		return this.http.get<T>(`/${this.getVersionString()}/${this.endpoint}/${uuid}`, { params });
+	}
+
+	public update(item: T, params?: {}): Observable<T> {
+		return this.http.put<T>(`/${this.getVersionString()}/${this.endpoint}/${item.uuid}`, item, { params });
+	}
+
+	public delete(uuid: string, params?: {}): Observable<T> {
+		return this.http.delete<T>(`/${this.getVersionString()}/${this.endpoint}/${uuid}`, { params });
+	}
+
+	public list(params?: {}): Observable<Array<T>> {
+		return this.http.get<Array<T>>(`/${this.getVersionString()}/${this.endpoint}`, { params });
+	}
+
+	public getVersionString(): string {
+		return `v${this.version}`;
+	}
+
+}
---
+++ projects/ng-dk-public-service/src/lib/service/http/abstract-sensor-session.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { AbstractSensorSessionDTOV3, AbstractSensorEventDTOV3 } from '@diamondkinetics/dk-public-dto-ts';
+import { AbstractSyncableDTOV2 } from '@diamondkinetics/dk-public-dto-ts';
+
+import { ResourceMappings as route } from '@lib/enum/resource-mappings.enum';
+import { AbstractResourceService } from './abstract-resource.service';
+
+export abstract class AbstractSensorSessionService<
+	EVENT extends AbstractSensorEventDTOV3,
+	T extends AbstractSyncableDTOV2|AbstractSensorSessionDTOV3<EVENT>
+> extends AbstractResourceService<T> {
+
+		constructor(protected http: HttpClient, protected version: number, protected endpoint: string) {
+			super(http, version, endpoint);
+		}
+
+		public createForSpecifiedUser(session: T, forUserUuid: string, groupUuid?: string, params?: {}): Observable<T> {
+			if (groupUuid) {
+				params = { ...params, groupUuid };
+			}
+
+			return this.http.post<T>(
+				`/${this.getVersionString()}/${route.USERS}/${forUserUuid}/${this.endpoint}`,
+				session,
+				{ params });
+		}
+
+}
