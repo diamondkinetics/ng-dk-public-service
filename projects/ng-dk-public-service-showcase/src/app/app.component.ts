721a48580194aacb7c51b59bc8c2c66c82083618
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-01-29 11:40:42 -0500

    Added app component auth logic.

--- projects/ng-dk-public-service-showcase/src/app/app.component.ts
+++ projects/ng-dk-public-service-showcase/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { AuthService } from 'projects/ng-dk-public-service/src/lib/service/http/auth/auth.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-dk-public-service-showcase';
+export class AppComponent implements OnInit {
+
+	public result$: Observable<string>;
+	public loginForm: FormGroup;
+	public emailOrNicknameFormControl: FormControl;
+	public passwordFormControl: FormControl;
+
+	constructor(private authService: AuthService, private formBuilder: FormBuilder) { }
+
+	ngOnInit() {
+		this.emailOrNicknameFormControl = new FormControl('', [Validators.required]);
+		this.passwordFormControl = new FormControl('', [Validators.required]);
+		this.loginForm = this.formBuilder.group({
+			emailOrNickname: this.emailOrNicknameFormControl,
+			password: this.passwordFormControl
+		});
+	}
+
+	public login(): void {
+		if (this.loginForm.invalid) {
+			return;
+		}
+
+		this.result$ = this.authService.login(
+			this.loginForm.controls.emailOrNickname.value,
+			this.loginForm.controls.password.value
+		).pipe(
+			map((xAuthToken: string): string => 'success'),
+			catchError((err: any): Observable<string> => of('failure'))
+		);
+	}
+
 }
