020271239c58189115440956881ba933fbd1cee5
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-07-14 09:28:37 -0400

    Modified the ResourceMappings enum to be a class version. Overrode toString() to print the path so there isn't a need to call getName when printing in template literals.

--- projects/ng-dk-public-service/src/lib/enum/resource-mappings.enum.ts
+++ projects/ng-dk-public-service/src/lib/enum/resource-mappings.enum.ts
@@ -1,6 +1,23 @@
-export enum ResourceMappings {
-	USERS = 'users',
-	GROUPS = 'groups',
-	BATTING_SESSIONS = 'battingSessions',
-	PITCHING_SESSIONS = 'pitchingSessions'
+export class ResourceMappings {
+
+	public static USERS = new ResourceMappings('users');
+	public static USER_PROFILE = new ResourceMappings(`${ResourceMappings.USERS}/profile`);
+	public static GROUPS = new ResourceMappings('groups');
+	public static BATTING_SESSIONS = 'battingSessions';
+	public static PITCHING_SESSIONS = 'pitchingSessions';
+
+	private readonly path: string;
+
+	constructor(path: string) {
+		this.path = path;
+	}
+
+	get getPath(): string {
+		return this.path;
+	}
+
+	public toString(): string {
+		return this.getPath;
+	}
+
 }
