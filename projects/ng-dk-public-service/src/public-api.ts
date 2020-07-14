2c636975b4f90d6eb580b93fddf7892fcce38d9c
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-07-14 15:09:25 -0400

    Added services to the api surface

--- projects/ng-dk-public-service/src/public-api.ts
+++ projects/ng-dk-public-service/src/public-api.ts
@@ -4,7 +4,11 @@
 
 export * from './lib/service/service.module';
 export * from './lib/service/http/auth/auth.module';
+export * from './lib/service/http/auth/auth.service';
 export * from './lib/service/http/v3/service.module.v3';
 export * from './lib/service/http/v3/user-profile/user-profile.module.v3';
+export * from './lib/service/http/v3/user-profile/user-profile.service.v3';
 export * from './lib/service/http/v3/batting-session/batting-session.module.v3';
+export * from './lib/service/http/v3/batting-session/batting-session.service.v3';
 export * from './lib/service/http/v3/pitching-session/pitching-session.module.v3';
+export * from './lib/service/http/v3/pitching-session/pitching-session.service.v3';
