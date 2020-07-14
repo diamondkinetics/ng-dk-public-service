c309077e4efa88d85499c2c590c1d5d37f008abc
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-07-14 13:11:21 -0400

    Angular 10 needs all modules exported from the public-api surface.

--- projects/ng-dk-public-service/src/public-api.ts
+++ projects/ng-dk-public-service/src/public-api.ts
@@ -3,3 +3,8 @@
  */
 
 export * from './lib/service/service.module';
+export * from './lib/service/http/auth/auth.module';
+export * from './lib/service/http/v3/service.module.v3';
+export * from './lib/service/http/v3/user-profile/user-profile.module.v3';
+export * from './lib/service/http/v3/batting-session/batting-session.module.v3';
+export * from './lib/service/http/v3/pitching-session/pitching-session.module.v3';
