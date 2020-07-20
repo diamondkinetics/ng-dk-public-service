b6997cbfd9794db5564fe0a9e031132c5cbb3d42
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-07-20 08:56:17 -0400

    Added some additional classes to the api surface.

--- projects/ng-dk-public-service/src/public-api.ts
+++ projects/ng-dk-public-service/src/public-api.ts
@@ -2,9 +2,12 @@
  * Public API Surface of ng-dk-public-service
  */
 
+export * from './lib/enum/resource-mappings.enum';
 export * from './lib/service/service.module';
 export * from './lib/service/http/auth/auth.module';
 export * from './lib/service/http/auth/auth.service';
+export * from './lib/service/http/abstract-resource.service';
+export * from './lib/service/http/abstract-sensor-session.service';
 export * from './lib/service/http/v3/service.module.v3';
 export * from './lib/service/http/v3/user-profile/user-profile.module.v3';
 export * from './lib/service/http/v3/user-profile/user-profile.service.v3';
