990302a9120ffeec8a6451b83f97ebe50076a66e
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-08-11 09:42:16 -0400

    Blah, forgot to add the new billing stuff to the api surface.

--- projects/ng-dk-public-service/src/public-api.ts
+++ projects/ng-dk-public-service/src/public-api.ts
@@ -28,3 +28,5 @@ export * from './lib/service/http/v3/pitching-session/pitching-session.service.v
 /////////////////////////////////////////////////////////////////////////////////////
 export * from './lib/service/http/v2/competition-level/competition-level.module.v2';
 export * from './lib/service/http/v2/competition-level/competition-level.service.v2';
+export * from './lib/service/http/v2/billing/billing-card.module.v2';
+export * from './lib/service/http/v2/billing/billing-card.service.v2';
