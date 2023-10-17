68bb683b7e7dc928e961543b6fb6ca22bb8f19bd
Eric Drobotij <edrobotij@diamondkinetics.com>
2023-10-17 14:51:02 -0400

    Added method for hitting swing score and streak endpoint.

--- projects/ng-dk-public-service/src/lib/service/http/v6/swing-score/swing-score-v6.service.ts
+++ projects/ng-dk-public-service/src/lib/service/http/v6/swing-score/swing-score-v6.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {
+  SwingScoreAndStreakResponseV6,
+  SwingScoreCollectionResponseV6,
+  SwingScoreResponseV6,
+} from '@diamondkinetics/dk-public-dto-ts';
+import { Observable } from 'rxjs';
 import { AbstractRequestResponseResourceService } from '../../abstract-request-response-resource.service';
 import { ResourceMapping } from '../../../../enum/resource-mapping.enum';
-import { SwingScoreCollectionResponseV6, SwingScoreResponseV6 } from '@diamondkinetics/dk-public-dto-ts';
 
 @Injectable({ providedIn: 'root' })
 export class SwingScoreV6Service extends AbstractRequestResponseResourceService<
@@ -13,5 +18,11 @@ export class SwingScoreV6Service extends AbstractRequestResponseResourceService<
 > {
   constructor(protected httpClient: HttpClient) {
     super(httpClient, 6, ResourceMapping.SWING_SCORES.getPath());
+  }
+
+  public getSwingScoreAndStreak(userUuid: string): Observable<SwingScoreAndStreakResponseV6> {
+    return this.httpClient.get<SwingScoreAndStreakResponseV6>(
+      `/v${this.versionNumber}/${ResourceMapping.USERS}/${userUuid}/swingPerformance`
+    );
   }
 }
