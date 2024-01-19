b2d546ac240b40e6e66a4a124698e71c48cb6fa1
Eric Drobotij <edrobotij@diamondkinetics.com>
2024-01-19 11:39:49 -0500

    Added a claimReward method for challenge progress that uses POST since our abstract update method uses PUT.

--- projects/ng-dk-public-service/src/lib/service/http/v6/challenge-progress/challenge-progress-v6.service.ts
+++ projects/ng-dk-public-service/src/lib/service/http/v6/challenge-progress/challenge-progress-v6.service.ts
@@ -6,12 +6,30 @@ import {
   ChallengeProgressResponseV6,
   ChallengeProgressUpdateRequestV6,
 } from '@diamondkinetics/dk-public-dto-ts';
+import { Observable } from 'rxjs';
 import { AbstractRequestResponseResourceService } from '../../abstract-request-response-resource.service';
 import { ResourceMapping } from '../../../../enum/resource-mapping.enum';
 
 @Injectable({ providedIn: 'root' })
-export class ChallengeProgressV6Service extends AbstractRequestResponseResourceService<ChallengeProgressCreateRequestV6, ChallengeProgressUpdateRequestV6, ChallengeProgressResponseV6, ChallengeProgressCollectionResponseV6> {
+export class ChallengeProgressV6Service extends AbstractRequestResponseResourceService<
+  ChallengeProgressCreateRequestV6,
+  ChallengeProgressUpdateRequestV6,
+  ChallengeProgressResponseV6,
+  ChallengeProgressCollectionResponseV6
+> {
   constructor(protected httpClient: HttpClient) {
     super(httpClient, 6, ResourceMapping.CHALLENGE_PROGRESS.getPath());
+  }
+
+  public claimReward(
+    updateRequest: ChallengeProgressUpdateRequestV6,
+    uuid: string,
+    params?: {}
+  ): Observable<ChallengeProgressResponseV6> {
+    return this.httpClient.post<ChallengeProgressResponseV6>(
+      `/v${this.versionNumber}/${this.endpoint}/${uuid}`,
+      updateRequest,
+      { params }
+    );
   }
 }
