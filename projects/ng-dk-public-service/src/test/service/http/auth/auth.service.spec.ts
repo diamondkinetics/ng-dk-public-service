99b0f55fedec4f63e56a3591499337fb7e8aaa52
Eric Drobotij <edrobotij@diamondkinetics.com>
2024-12-03 10:31:27 -0500

    Updates angular core, cli, and eslint packages to v18.

--- projects/ng-dk-public-service/src/test/service/http/auth/auth.service.spec.ts
+++ projects/ng-dk-public-service/src/test/service/http/auth/auth.service.spec.ts
@@ -1,11 +1,14 @@
-import { HTTP_INTERCEPTORS, HttpResponse, HttpHeaders } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-
 import {
-	RegisterWithProfileRequestDTOV2,
-	UserProfileDTOV2
-} from '@diamondkinetics/dk-public-dto-ts';
+  HTTP_INTERCEPTORS,
+  HttpResponse,
+  HttpHeaders,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { TestBed } from '@angular/core/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+
+import { RegisterWithProfileRequestDTOV2, UserProfileDTOV2 } from '@diamondkinetics/dk-public-dto-ts';
 
 import { RequestInterceptor } from '~test/util/request-interceptor/request.interceptor';
 import { environment } from '~test/environments/environment';
@@ -13,72 +16,73 @@ import { AuthService } from '~lib/service/http/auth/auth.service';
 import { mockRegisterWithProfileRequestDTOV2 } from '~test/mock/v2/register-with-profile-request.dto.v2.mock';
 import { mockUserProfileDTOV2 } from '~test/mock/v2/user-profile.dto.v2.mock';
 
-
 describe('AuthService', () => {
-	let httpTestingController: HttpTestingController;
-	let service: AuthService;
-	let mockRegisterRequest: RegisterWithProfileRequestDTOV2;
-	let mockProfile: UserProfileDTOV2;
+  let httpTestingController: HttpTestingController;
+  let service: AuthService;
+  let mockRegisterRequest: RegisterWithProfileRequestDTOV2;
+  let mockProfile: UserProfileDTOV2;
 
-	beforeEach(() => {
-		TestBed.configureTestingModule({
-			providers: [
-				AuthService,
-				{
-					provide: HTTP_INTERCEPTORS,
-					useClass: RequestInterceptor,
-					multi: true
-				}
-			],
-			imports: [HttpClientTestingModule]
-		});
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [
+        AuthService,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: RequestInterceptor,
+          multi: true,
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+      ],
+    });
 
-		httpTestingController = TestBed.inject(HttpTestingController);
-		service = TestBed.inject(AuthService);
-		mockRegisterRequest = mockRegisterWithProfileRequestDTOV2();
-		mockProfile = mockUserProfileDTOV2();
-	});
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(AuthService);
+    mockRegisterRequest = mockRegisterWithProfileRequestDTOV2();
+    mockProfile = mockUserProfileDTOV2();
+  });
 
-	afterEach(() => {
-		httpTestingController.verify();
-	});
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
-	it('Should be created', () => {
-		expect(service).toBeTruthy();
-	});
+  it('Should be created', () => {
+    expect(service).toBeTruthy();
+  });
 
-	describe('login()', () => {
-		it('Should POST the correct auth data and return an X-AUTH-TOKEN', () => {
-			const mockXAuthToken = 'thisIsSuperSecretGuys';
-			const mockResponseHeaders: HttpHeaders = new HttpHeaders({
-				'X-AUTH-TOKEN': mockXAuthToken
-			});
-			const mockResponse: HttpResponse<any> = new HttpResponse<any>({
-				body: 'Ur logged in mayne',
-				headers: mockResponseHeaders
-			});
+  describe('login()', () => {
+    it('Should POST the correct auth data and return an X-AUTH-TOKEN', () => {
+      const mockXAuthToken = 'thisIsSuperSecretGuys';
+      const mockResponseHeaders: HttpHeaders = new HttpHeaders({
+        'X-AUTH-TOKEN': mockXAuthToken,
+      });
+      const mockResponse: HttpResponse<any> = new HttpResponse<any>({
+        body: 'Ur logged in mayne',
+        headers: mockResponseHeaders,
+      });
 
-			service.login('usernameOrEmail', 'password').subscribe((xAuthToken: string) => {
-				expect(xAuthToken).toEqual(mockXAuthToken);
-			});
+      service.login('usernameOrEmail', 'password').subscribe((xAuthToken: string) => {
+        expect(xAuthToken).toEqual(mockXAuthToken);
+      });
 
-			const req = httpTestingController.expectOne(`${environment.apiUrl}/api/user/login`);
-			expect(req.request.method).toEqual('POST');
-			req.flush(mockResponse.body, { headers: mockResponse.headers });
-		});
-	});
+      const req = httpTestingController.expectOne(`${environment.apiUrl}/api/user/login`);
+      expect(req.request.method).toEqual('POST');
+      req.flush(mockResponse.body, { headers: mockResponse.headers });
+    });
+  });
 
-	describe('logout()', () => {
-		it('Should POST nothing and return an HttpResponse', () => {
-			const mockResponse: HttpResponse<{}> = new HttpResponse<{}>({});
+  describe('logout()', () => {
+    it('Should POST nothing and return an HttpResponse', () => {
+      const mockResponse: HttpResponse<{}> = new HttpResponse<{}>({});
 
-			service.logout().subscribe((response: HttpResponse<{}>) => {
-				expect(response).toEqual(mockResponse);
-			});
+      service.logout().subscribe((response: HttpResponse<{}>) => {
+        expect(response).toEqual(mockResponse);
+      });
 
-			const req = httpTestingController.expectOne(`${environment.apiUrl}/api/user/logout`);
-			expect(req.request.method).toEqual('POST');
-			req.flush(mockResponse);
-		});
-	});
+      const req = httpTestingController.expectOne(`${environment.apiUrl}/api/user/logout`);
+      expect(req.request.method).toEqual('POST');
+      req.flush(mockResponse);
+    });
+  });
 });
--- projects/ng-dk-public-service/src/test/service/http/resource.service.test-suite.spec.ts
+++ projects/ng-dk-public-service/src/test/service/http/resource.service.test-suite.spec.ts
@@ -1,7 +1,7 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { Provider } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { AbstractSyncableDTO } from '@diamondkinetics/dk-public-dto-ts';
 
 import { AbstractResourceService } from '~lib/service/http/abstract-resource.service';
@@ -9,181 +9,175 @@ import { RequestInterceptor } from '~test/util/request-interceptor/request.inter
 import { environment } from '~test/environments/environment';
 
 export abstract class ResourceServiceTestSuite<T extends AbstractSyncableDTO, S extends AbstractResourceService<T>> {
+  protected service: S;
+  protected providers: Provider[] = [];
+  protected httpTestingController: HttpTestingController;
 
-	protected service: S;
-	protected providers: Provider[] = [];
-	protected httpTestingController: HttpTestingController;
+  protected expectGetRequest: (method: string) => void;
+  protected expectPostRequest: (method: string) => void;
+  protected expectPutRequest: (method: string) => void;
+  protected expectDeleteRequest: (method: string) => void;
 
-	protected expectGetRequest: (method: string) => void;
-	protected expectPostRequest: (method: string) => void;
-	protected expectPutRequest: (method: string) => void;
-	protected expectDeleteRequest: (method: string) => void;
+  constructor(
+    private readonly resourceRoute: string,
+    private readonly resourceClass: string,
+    protected mockResource: T
+  ) {
+    this.providers.push(this.getRequestInterceptorProvider());
+    this.expectGetRequest = (method: string) => expect(method).toEqual('GET');
+    this.expectPostRequest = (method: string) => expect(method).toEqual('POST');
+    this.expectPutRequest = (method: string) => expect(method).toEqual('PUT');
+    this.expectDeleteRequest = (method: string) => expect(method).toEqual('DELETE');
+  }
 
-	constructor(
-		private readonly resourceRoute: string,
-		private readonly resourceClass: string,
-		protected mockResource: T
-	) {
-		this.providers.push(this.getRequestInterceptorProvider());
-		this.expectGetRequest = (method: string) => expect(method).toEqual('GET');
-		this.expectPostRequest = (method: string) => expect(method).toEqual('POST');
-		this.expectPutRequest = (method: string) => expect(method).toEqual('PUT');
-		this.expectDeleteRequest = (method: string) => expect(method).toEqual('DELETE');
-	}
+  public run(): void {
+    const dtoMatcher = (result: T) => expect(this.mockResource).toEqual(result);
+    const uuidMatcher = (uuid: string) => expect(this.mockResource.uuid).toEqual(uuid);
+    const dtoDeletedMatcher = (dto: T) => expect(dto.deleted).toEqual(true);
+    const listMatcher = (dtoList: T[]) =>
+      expect([this.mockResource, this.mockResource, this.mockResource]).toEqual(dtoList);
 
-	public run(): void {		
-		const dtoMatcher = (result: T) => expect(this.mockResource).toEqual(result);
-		const uuidMatcher = (uuid: string) => expect(this.mockResource.uuid).toEqual(uuid);
-		const dtoDeletedMatcher = (dto: T) => expect(dto.deleted).toEqual(true);
-		const listMatcher = (dtoList: T[]) => expect([this.mockResource, this.mockResource, this.mockResource])
-			.toEqual(dtoList);
-		
-		beforeEach(() => {
-			TestBed.configureTestingModule({
-				providers: this.providers,
-				imports: [HttpClientTestingModule]
-			});
-	
-			this.httpTestingController = TestBed.inject(HttpTestingController);
-			this.service = this.getInjectedService();
-		});
-	
-		afterEach(() => {
-			this.httpTestingController.verify();
-		});
-	
-		it('Should be created', () => {
-			expect(this.getInjectedService()).toBeTruthy();
-		});
-	
-		describe('create()', () => {
-			it(`Should POST the correct ${this.resourceClass}`, () => {
-				this.testCreate(dtoMatcher, this.expectPostRequest);
-			});
-		});
-	
-		describe('readForActingUser()', () => {
-			it(`Should GET the correct ${this.resourceClass}`, () => {
-				this.testReadForActingUser(dtoMatcher, this.expectGetRequest);
-			});
-		});
-	
-		describe('read()', () => {
-			it(`Should GET the correct ${this.resourceClass}`, () => {
-				this.testRead(uuidMatcher, this.expectGetRequest);
-			});
-		});
-	
-		describe('update()', () => {
-			it(`Should PUT the correct ${this.resourceClass}`, () => {
-				this.testUpdate(dtoMatcher, this.expectPutRequest);
-			});
-		});
-	
-		describe('delete()', () => {
-			it(`Should DELETE the correct ${this.resourceClass}`, () => {
-				this.testDelete(dtoDeletedMatcher, this.expectDeleteRequest);
-			});
-		});
-	
-		describe('list()', () => {
-			it(`Should GET the correct ${this.resourceClass} collection`, () => {
-				this.testList(listMatcher, this.expectGetRequest);
-			});
-		});
-	}
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [],
+        providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()],
+      });
 
-	private testCreate(dtoMatcher: (created: T) => void, requestMatcher: (method: string) => void): void {
-		this.service.create(this.mockResource).subscribe((created: T): void => {
-			dtoMatcher(created);
-		});
+      this.httpTestingController = TestBed.inject(HttpTestingController);
+      this.service = this.getInjectedService();
+    });
 
-		const req = this.httpTestingController.expectOne(
-			`${environment.apiUrl}/${this.service.getVersionString()}/${this.resourceRoute}`
-		);
-		requestMatcher(req.request.method);
-		req.flush(this.mockResource);
-	}
+    afterEach(() => {
+      this.httpTestingController.verify();
+    });
 
-	private testReadForActingUser(dtoMatcher: (created: T) => void, requestMatcher: (method: string) => void): void {
-		this.service.readForActingUser().subscribe((dto: T): void => {
-			dtoMatcher(dto);
-		});
+    it('Should be created', () => {
+      expect(this.getInjectedService()).toBeTruthy();
+    });
 
-		const req = this.httpTestingController.expectOne(
-			`${environment.apiUrl}/${this.service.getVersionString()}/${this.resourceRoute}`
-		);
-		requestMatcher(req.request.method);
-		req.flush(this.mockResource);
-	}
+    describe('create()', () => {
+      it(`Should POST the correct ${this.resourceClass}`, () => {
+        this.testCreate(dtoMatcher, this.expectPostRequest);
+      });
+    });
 
-	private testRead(uuidMatcher: (uuid: string) => void, requestMatcher: (method: string) => void): void {
-		this.service.read(this.mockResource.uuid).subscribe((dto: T): void => {
-			uuidMatcher(dto.uuid);
-		});
+    describe('readForActingUser()', () => {
+      it(`Should GET the correct ${this.resourceClass}`, () => {
+        this.testReadForActingUser(dtoMatcher, this.expectGetRequest);
+      });
+    });
 
-		const req = this.httpTestingController.expectOne(
-			`${environment.apiUrl}/${this.service.getVersionString()}/${this.resourceRoute}/${this.mockResource.uuid}`
-		);
-		requestMatcher(req.request.method);
-		req.flush(this.mockResource);
-	}
+    describe('read()', () => {
+      it(`Should GET the correct ${this.resourceClass}`, () => {
+        this.testRead(uuidMatcher, this.expectGetRequest);
+      });
+    });
 
-	private testUpdate(dtoMatcher: (updated: T) => void, requestMatcher: (method: string) => void): void {
-		this.service.update(this.mockResource).subscribe((dto: T): void => {
-			dtoMatcher(dto);
-		});
+    describe('update()', () => {
+      it(`Should PUT the correct ${this.resourceClass}`, () => {
+        this.testUpdate(dtoMatcher, this.expectPutRequest);
+      });
+    });
 
-		const req = this.httpTestingController.expectOne(
-			`${environment.apiUrl}/${this.service.getVersionString()}/${this.resourceRoute}/${this.mockResource.uuid}`
-		);
-		requestMatcher(req.request.method);
-		req.flush(this.mockResource);
-	}
+    describe('delete()', () => {
+      it(`Should DELETE the correct ${this.resourceClass}`, () => {
+        this.testDelete(dtoDeletedMatcher, this.expectDeleteRequest);
+      });
+    });
 
-	private testDelete(deletedMatcher: (deleted: T) => void, requestMatcher: (method: string) => void): void {
-		const deleted: T = {
-			...this.mockResource,
-			deleted: true
-		};
+    describe('list()', () => {
+      it(`Should GET the correct ${this.resourceClass} collection`, () => {
+        this.testList(listMatcher, this.expectGetRequest);
+      });
+    });
+  }
 
-		this.service.delete(this.mockResource.uuid).subscribe((dto: T): void => {
-			deletedMatcher(dto);
-		});
+  private testCreate(dtoMatcher: (created: T) => void, requestMatcher: (method: string) => void): void {
+    this.service.create(this.mockResource).subscribe((created: T): void => {
+      dtoMatcher(created);
+    });
 
-		const req = this.httpTestingController.expectOne(
-			`${environment.apiUrl}/${this.service.getVersionString()}/${this.resourceRoute}/${deleted.uuid}`
-		);
-		requestMatcher(req.request.method);
-		req.flush(deleted);
-	}
+    const req = this.httpTestingController.expectOne(
+      `${environment.apiUrl}/${this.service.getVersionString()}/${this.resourceRoute}`
+    );
+    requestMatcher(req.request.method);
+    req.flush(this.mockResource);
+  }
 
-	private testList(listMatcher: (dtoList: T[]) => void, requestMatcher: (method: string) => void): void {
-		const dtoList: T[] = [
-			this.mockResource,
-			this.mockResource,
-			this.mockResource
-		];
+  private testReadForActingUser(dtoMatcher: (created: T) => void, requestMatcher: (method: string) => void): void {
+    this.service.readForActingUser().subscribe((dto: T): void => {
+      dtoMatcher(dto);
+    });
 
-		this.service.list().subscribe((dtoResults: T[]): void => {
-			listMatcher(dtoResults);
-		});
+    const req = this.httpTestingController.expectOne(
+      `${environment.apiUrl}/${this.service.getVersionString()}/${this.resourceRoute}`
+    );
+    requestMatcher(req.request.method);
+    req.flush(this.mockResource);
+  }
 
-		const req = this.httpTestingController.expectOne(
-			`${environment.apiUrl}/${this.service.getVersionString()}/${this.resourceRoute}`
-		);
-		requestMatcher(req.request.method);
-		req.flush(dtoList);
-	}
+  private testRead(uuidMatcher: (uuid: string) => void, requestMatcher: (method: string) => void): void {
+    this.service.read(this.mockResource.uuid).subscribe((dto: T): void => {
+      uuidMatcher(dto.uuid);
+    });
 
-	private getRequestInterceptorProvider(): Provider {
-		return {
-			provide: HTTP_INTERCEPTORS,
-			useClass: RequestInterceptor,
-			multi: true
-		};
-	}
+    const req = this.httpTestingController.expectOne(
+      `${environment.apiUrl}/${this.service.getVersionString()}/${this.resourceRoute}/${this.mockResource.uuid}`
+    );
+    requestMatcher(req.request.method);
+    req.flush(this.mockResource);
+  }
 
-	protected abstract getInjectedService(): S;
+  private testUpdate(dtoMatcher: (updated: T) => void, requestMatcher: (method: string) => void): void {
+    this.service.update(this.mockResource).subscribe((dto: T): void => {
+      dtoMatcher(dto);
+    });
 
+    const req = this.httpTestingController.expectOne(
+      `${environment.apiUrl}/${this.service.getVersionString()}/${this.resourceRoute}/${this.mockResource.uuid}`
+    );
+    requestMatcher(req.request.method);
+    req.flush(this.mockResource);
+  }
+
+  private testDelete(deletedMatcher: (deleted: T) => void, requestMatcher: (method: string) => void): void {
+    const deleted: T = {
+      ...this.mockResource,
+      deleted: true,
+    };
+
+    this.service.delete(this.mockResource.uuid).subscribe((dto: T): void => {
+      deletedMatcher(dto);
+    });
+
+    const req = this.httpTestingController.expectOne(
+      `${environment.apiUrl}/${this.service.getVersionString()}/${this.resourceRoute}/${deleted.uuid}`
+    );
+    requestMatcher(req.request.method);
+    req.flush(deleted);
+  }
+
+  private testList(listMatcher: (dtoList: T[]) => void, requestMatcher: (method: string) => void): void {
+    const dtoList: T[] = [this.mockResource, this.mockResource, this.mockResource];
+
+    this.service.list().subscribe((dtoResults: T[]): void => {
+      listMatcher(dtoResults);
+    });
+
+    const req = this.httpTestingController.expectOne(
+      `${environment.apiUrl}/${this.service.getVersionString()}/${this.resourceRoute}`
+    );
+    requestMatcher(req.request.method);
+    req.flush(dtoList);
+  }
+
+  private getRequestInterceptorProvider(): Provider {
+    return {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true,
+    };
+  }
+
+  protected abstract getInjectedService(): S;
 }
